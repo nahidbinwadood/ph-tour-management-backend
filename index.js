06085680819c69c0f39670508ee6dac3d401dcfe
nahidbinwadood <nahidbinwadood@gmail.com>
2026-05-05 00:07:42 +0600

    feat: refactor root route response formatting and add index.js with sample data

---
+++ index.js
@@ -0,0 +1,12 @@
+const person1 = {
+  name: 'nahid bin wadood',
+  designation: 'software engineer',
+};
+
+const person2 = {
+  name: 'nabil dev',
+  designation: 'Senior Frontend Engineer',
+};
+
+console.log(person1);
+console.log(person2);
